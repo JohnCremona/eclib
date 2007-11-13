d7b3c07609a09cf7ef27983868308b9083cad99f
John Cremona <john.cremona@gmail.com>
2007-11-13 11:12:27 +0000

    Begun to remove redundant source files (done in proces so far)

--- qcurves/esign.h
+++
@@ -1,6 +0,0 @@
-//  file esign.cc
-//
-// Sign of functional equation for an elliptic curve over Q
-//
-int LocalRootNumber(const CurveRed& E, const bigint& p);
-int GlobalRootNumber(const Curvedata e);
