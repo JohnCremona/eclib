b393e9ee45472a8011848e002e57c847d18489f8
John Cremona <john.cremona@gmail.com>
2007-11-13 14:03:16 +0000

    Removed redundant files, cleaned up makefile, GPL's rest

--- qrank/silver.h
+++
@@ -1,8 +0,0 @@
-double silverman_bound(const Curvedata& CD);
-double siksek_bound(const Curvedata& CD);
-inline double height_constant(const Curvedata& CD)
-{
-  //  return silverman_bound(CD);
-  double b1 = silverman_bound(CD), b2 = siksek_bound(CD);
-  return min(b1,b2);
-}
--- qrank/tred.cc
+++ qrank/tred.cc
@@ -1,4 +1,25 @@
-// TREDUCE.CC:  Test program for reduction of quartics to minimal I,J
+// TRED.CC:  Test program for reduction of quartics to minimal I,J
+//////////////////////////////////////////////////////////////////////////
+//
+// Copyright 1990-2005 John Cremona
+// 
+// This file is part of the mwrank package.
+// 
+// mwrank is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+// 
+// mwrank is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with mwrank; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+//////////////////////////////////////////////////////////////////////////
 //    
 #include "marith.h"
 #include "unimod.h"
