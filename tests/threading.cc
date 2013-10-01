1f9939c408fadf45d4e85dfd51b41400acb49919
Marcus Mo <marcusmo88@gmail.com>
2013-10-01 10:56:21 +0100

    Added GPL licensing to new files

--- tests/threading.cc
+++ tests/threading.cc
@@ -1,11 +1,28 @@
-/**
- * threading.cc
- *
- * Simple program to test threadpool class
- */
+// FILE threading.cc : Simple test program for threadpool class
+//////////////////////////////////////////////////////////////////////////
+//
+// Copyright 1990-2012 Marcus Mo
+// 
+// This file is part of the eclib package.
+// 
+// eclib is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+// 
+// eclib is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with eclib; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+// 
+//////////////////////////////////////////////////////////////////////////
 
 // Enable/disable multithreading
-//#undef MULTITHREAD
+// #undef MULTITHREAD
 
 // Include headers
 #include <iostream>
