f87108a3ee46ecc8a5fb646a70380c5348458fe3
John Cremona <john.cremona@gmail.com>
2019-03-01 14:59:15 +0000

    increased default PARI stack size to 10^9 bytes (was only 100k)

--- libsrc/parifact.cc
+++ libsrc/parifact.cc
@@ -35,12 +35,14 @@ string
 factor(const string n)
 {
   if (!avma) {
-    long pari_size = strtol(getenv_with_default("PARI_SIZE", "100000").c_str(), NULL, 0);
+    long pari_size = strtol(getenv_with_default("PARI_SIZE", "1000000000").c_str(), NULL, 0);
     if (pari_size==0) // e.g. syntax error in the environment variable PARI_SIZE
-      pari_size = 1000000;
+      pari_size = 1000000000;
 #ifdef DEBUG_GPFACT
     std::cout<<"calling pari_init with pari_size = "<<pari_size<<endl;
 #endif
+    // the first parameter is the maximum stack size in bytes
+    // the second parameter is the maximum precomputed prime
     pari_init(pari_size, 1000000);
   }
 #ifdef DEBUG_GPFACT
@@ -65,12 +67,14 @@ int
 is_prime(const string p)
 {
   if (!avma) {
-    long pari_size = strtol(getenv_with_default("PARI_SIZE", "100000").c_str(), NULL, 0);
+    long pari_size = strtol(getenv_with_default("PARI_SIZE", "1000000000").c_str(), NULL, 0);
 #ifdef DEBUG_GPFACT
     std::cout<<"calling pari_init with pari_size = "<<pari_size<<endl;
 #endif
     if (pari_size==0) // e.g. syntax error in the environment variable PARI_SIZE
-      pari_size = 1000000;
+      pari_size = 1000000000;
+    // the first parameter is the maximum stack size in bytes
+    // the second parameter is the maximum precomputed prime
     pari_init(pari_size, 1000000);
   }
   pari_sp av=avma;  // store pari stack pointer
