b3c647c9619d888e638adba5fce8a82571e93c98
John Cremona <john.cremona@gmail.com>
2025-10-28 14:37:22 +0000

    more matrix conversion functions

--- libsrc/eclib/matrix.h
+++ libsrc/eclib/matrix.h
@@ -184,14 +184,69 @@ template<class T>
 mat_ZZ_p mat_ZZ_p_from_mat(const Zmat<T>& M, const T& pr);
 
 // Construct a mat (T type same as pr) from an NTL mat_lzz_p
-
 template<class T>
 Zmat<T> mat_from_mat_ZZ_p(const mat_ZZ_p& A, const T& pr); // type of T fixes return type
 
-mat_m to_mat_m(const mat_i& v);
-mat_m to_mat_m(const mat_l& v);
-mat_i to_mat_i(const mat_m& v);
-mat_l to_mat_l(const mat_m& v);
+// convert between matrices over different integer types
+template<class T>
+mat_m to_mat_m(const Zmat<T>& M);
+template<class T>
+mat_i to_mat_i(const Zmat<T>& M);
+template<class T>
+mat_l to_mat_l(const Zmat<T>& M);
+
+// convert an eclib Zmat to an NTL mat_ZZ:
+template<class T>
+mat_ZZ mat_to_mat_ZZ(Zmat<T> A);
+
+// convert an eclib Zmat to an NTL mat_ZZ_p:
+template<class T>
+mat_ZZ_p mat_to_mat_ZZ_p(Zmat<T> A);
+
+// compute char poly of A:
+ZZX charpoly(const mat_ZZ& A);
+
+// compute char poly of A:
+template<class T>
+ZZX charpoly(const Zmat<T>& A);
+
+// compute char poly of A/den mod m:
+ZZX scaled_charpoly(const mat_ZZ& A, const ZZ& den = to_ZZ(1), const ZZ& m = to_ZZ(0));
+
+// return A mod m (or just A if m==0)
+mat_ZZ reduce_mat(const mat_ZZ& A, const ZZ& m);
+
+// evaluate f(A) (assumes f monic)
+mat_ZZ evaluate(const ZZX& f, const mat_ZZ& A);
+// evaluate f(A) (assumes f monic)
+template<class T>
+mat_m evaluate(const ZZX& f, const Zmat<T>& A);
+
+// p should be monic:
+mat_ZZ CompanionMatrix(const ZZX& p);
+
+// check that a matrix is a scaled involution (modulo m, if m!=0):
+int check_involution(const mat_ZZ& A, const ZZ& den, const ZZ& m, int verbose=0);
+
+// check that two matrices commute (modulo m, if m!=0):
+int commute(const mat_ZZ& A, const mat_ZZ& B, const ZZ& m);
+
+// check that a matrix commutes (modulo m, if m!=0) with all those in a list:
+int check_commute(const mat_ZZ& A, const vector<mat_ZZ>& Blist, const ZZ& m);
+// rank of an NTL matrix:
+long rank(mat_ZZ A);
+
+// nullity of an NTL matrix:
+long nullity(mat_ZZ A);
+
+// Linear combinarion of n>0 matrices, all dxd
+mat_m lin_comb_mats(const vec_m& co, const vector<mat_m>& mats);
+// Linear combinarion of n>0 matrices, all dxd
+mat_m lin_comb_mats(const vector<ZZ>& co, const vector<mat_m>& mats);
+
+// same as m.output(cout) except no newlines between rows
+template<class T>
+void output_flat_matrix(const Zmat<T>& m, ostream&s = cout);
 
 #endif
 
--- libsrc/matrix.cc
+++ libsrc/matrix.cc
@@ -23,6 +23,7 @@
  
 #include "eclib/convert.h"
 #include "eclib/linalg.h"
+#include "eclib/polys.h"
 
 // Instantiate Zmat template classes for T=int, long, ZZ
 
@@ -1575,49 +1576,51 @@ double sparsity(const Zmat<T>& m)
   return double(population(m))/m.entries.size();
 }
 
-
-mat_m to_mat_m(const mat_i& m)
+template<class T>
+mat_m to_mat_m(const Zmat<T>& M)
 {
-  const vector<int> & mij = m.get_entries();
-  vector<ZZ> n(mij.size());
-  std::transform(mij.begin(), mij.end(), n.begin(), [](const int& x) {return ZZ(x);});
-  return mat_m(m.nrows(), m.ncols(), n);
+  const vector<T> & Mij = M.get_entries();
+  vector<ZZ> entries(Mij.size());
+  std::transform(Mij.begin(), Mij.end(), entries.begin(), [](const T& x) {return to_ZZ(x);});
+  return mat_m(M.nrows(), M.ncols(), entries);
 }
+template mat_m to_mat_m(const Zmat<int>& M);
+template mat_m to_mat_m(const Zmat<long>& M);
+template mat_m to_mat_m(const Zmat<ZZ>& M);
 
-mat_m to_mat_m(const mat_l& m)
-{
-  const vector<long> & mij = m.get_entries();
-  vector<ZZ> n(mij.size());
-  std::transform(mij.begin(), mij.end(), n.begin(), [](const long& x) {return ZZ(x);});
-  return mat_m(m.nrows(), m.ncols(), n);
-}
+int to_int(const int& i) {return i;}
+int to_int(const long& i) {return i;}
+// NTL has a to_int function for ZZ -> int
 
-mat_i to_mat_i(const mat_m& m)
+template<class T>
+mat_i to_mat_i(const Zmat<T>& M)
 {
-  const vector<ZZ> & mij = m.get_entries();
-  auto toint = [](const ZZ& a) {return is_int(a)? I2int(a) : int(0);};
-  vector<int> n(mij.size());
-  std::transform(mij.begin(), mij.end(), n.begin(), toint);
-  return mat_i(m.nrows(), m.ncols(), n);
+  const vector<T> & Mij = M.get_entries();
+  vector<int> entries(Mij.size());
+  std::transform(Mij.begin(), Mij.end(), entries.begin(),
+                 [](const T& i){return to_int(i);});
+  return mat_i(M.nrows(), M.ncols(), entries);
 }
+template mat_i to_mat_i(const Zmat<int>& M);
+template mat_i to_mat_i(const Zmat<long>& M);
+template mat_i to_mat_i(const Zmat<ZZ>& M);
 
-mat_l to_mat_l(const mat_m& m)
-{
-  const vector<ZZ> & mij = m.get_entries();
-  auto tolong = [](const ZZ& a) {return is_long(a)? I2long(a) : long(0);};
-  vector<long> n(mij.size());
-  std::transform(mij.begin(), mij.end(), n.begin(), tolong);
-  return mat_l(m.nrows(), m.ncols(), n);
-}
+long to_long(const int& i) {return i;}
+long to_long(const long& i) {return i;}
+// NTL has a to_long function for ZZ -> long
 
-mat_l to_mat_l(const mat_i& m)
+template<class T>
+mat_l to_mat_l(const Zmat<T>& M)
 {
-  const vector<int> & mij = m.get_entries();
-  auto tolong = [](const int& a) {return long(a);};
-  vector<long> n(mij.size());
-  std::transform(mij.begin(), mij.end(), n.begin(), tolong);
-  return mat_l(m.nrows(), m.ncols(), n);
+  const vector<T> & Mij = M.get_entries();
+  vector<long> entries(Mij.size());
+  std::transform(Mij.begin(), Mij.end(), entries.begin(),
+                 [](const T& i){return to_long(i);});
+  return mat_l(M.nrows(), M.ncols(), entries);
 }
+template mat_l to_mat_l(const Zmat<int>& M);
+template mat_l to_mat_l(const Zmat<long>& M);
+template mat_l to_mat_l(const Zmat<ZZ>& M);
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -1820,6 +1823,214 @@ Zmat<T> ref_via_flint(const Zmat<T>& M, Zvec<int>& pcols, Zvec<int>& npcols,
   return R;
 }
 
+template<class T>
+mat_ZZ mat_to_mat_ZZ(Zmat<T> A)
+{
+  int d = A.nrows();
+  // copy into an NTL matrix:
+  mat_ZZ ntl_A;
+  ntl_A.SetDims(d,d);
+  for(int i=1; i<=d; i++)
+    for(int j=1; j<=d; j++)
+      ntl_A(i,j)=NTL::conv<ZZ>(A(i,j));
+  return ntl_A;
+}
+template mat_ZZ mat_to_mat_ZZ<int>(Zmat<int> A);
+template mat_ZZ mat_to_mat_ZZ<long>(Zmat<long> A);
+template mat_ZZ mat_to_mat_ZZ<ZZ>(Zmat<ZZ> A);
+
+template<class T>
+mat_ZZ_p mat_to_mat_ZZ_p(Zmat<T> A)
+{
+  int d = A.nrows();
+
+  // copy into an NTL matrix:
+  mat_ZZ_p ntl_A;
+  ntl_A.SetDims(d,d);
+  for(int i=1; i<=d; i++)
+    for(int j=1; j<=d; j++)
+      ntl_A(i,j)=NTL::conv<ZZ_p>(A(i,j));
+  return ntl_A;
+}
+
+template mat_ZZ_p mat_to_mat_ZZ_p<int>(Zmat<int> A);
+template mat_ZZ_p mat_to_mat_ZZ_p<long>(Zmat<long> A);
+template mat_ZZ_p mat_to_mat_ZZ_p<ZZ>(Zmat<ZZ> A);
+
+// compute char poly of A:
+ZZX charpoly(const mat_ZZ& A)
+{
+  ZZX charpol;
+  CharPoly(charpol, A);
+  return charpol;
+}
+
+// compute char poly of A:
+template<class T>
+ZZX charpoly(const Zmat<T>& A)
+{
+  return charpoly(mat_to_mat_ZZ(A));
+}
+
+template ZZX charpoly<int>(const Zmat<int>& A);
+template ZZX charpoly<long>(const Zmat<long>& A);
+template ZZX charpoly<ZZ>(const Zmat<ZZ>& A);
+
+ZZX scaled_charpoly(const mat_ZZ& A, const ZZ& den, const ZZ& m)
+{
+  ZZX charpol;
+  CharPoly(charpol, A);
+  return reduce_poly(scale_poly_down(charpol, den), m);
+}
+
+// return A mod m (or just A if m==0)
+mat_ZZ reduce_mat(const mat_ZZ& A, const ZZ& m)
+{
+  if (m==0) return A;
+  int nr= A.NumRows(), nc = A.NumCols();
+  mat_ZZ B = A;
+  for (int i=1; i<=nr; i++)
+    for (int j=1; j<=nc; j++)
+      B(i,j) = mod(B(i,j), m);
+  return B;
+}
+
+// evaluate f(A) (assumes f monic)
+mat_ZZ evaluate(const ZZX& f, const mat_ZZ& A)
+{
+  long d = deg(f);
+  mat_ZZ fA = A, I;
+  ident(I, A.NumRows());
+  for(int i=d-1; i>=0; i--)
+    {
+      fA += coeff(f,i)*I;
+      if(i)
+        fA *= A;
+    }
+  return fA;
+}
+
+// evaluate f(A) (assumes f monic)
+template<class T>
+mat_m evaluate(const ZZX& f, const Zmat<T>& A)
+{
+  long d = deg(f);
+  mat_m mA(to_mat_m(A));
+  mat_m fA(mA);
+  for(int i=d-1; i>=0; i--)
+    {
+      fA = addscalar(fA, coeff(f,i));
+      if(i)
+        fA = fA*mA;
+    }
+  return fA;
+}
+
+template mat_m evaluate<int>(const ZZX& f, const Zmat<int>& A);
+template mat_m evaluate<long>(const ZZX& f, const Zmat<long>& A);
+template mat_m evaluate<ZZ>(const ZZX& f, const Zmat<ZZ>& A);
+
+// p should be monic:
+mat_ZZ CompanionMatrix(const ZZX& p)
+{
+  int d = deg(p);
+  mat_ZZ A;
+  A.SetDims(d,d);
+  ZZ one(1);
+  for(int i=1; i<d; i++)
+    {
+      A(i+1,i) = one;
+      A(i,d) = -coeff(p, i-1);
+    }
+  A(d,d) = -coeff(p, d-1);
+  return A;
+}
+
+int check_involution(const mat_ZZ& A, const ZZ& den, const ZZ& m, int verbose)
+{
+  int res = IsDiag(reduce_mat(sqr(A), to_ZZ(m)), A.NumRows(), to_ZZ(den*den));
+  if (verbose)
+    cout << (res? "Involution!": "NOT an involution....") << "\n";
+  return res;
+}
+
+int commute(const mat_ZZ& A, const mat_ZZ& B, const ZZ& m)
+{
+  return IsZero(reduce_mat(A*B-B*A, to_ZZ(m)));
+}
+
+// check that a matrix commutes with all those in a list:
+int check_commute(const mat_ZZ& A, const vector<mat_ZZ>& Blist, const ZZ& modulus)
+{
+  return std::all_of(Blist.begin(), Blist.end(), [A, modulus] (const mat_ZZ& B) {return commute(A,B,modulus);});
+}
+
+// Linear combinarion of n>0 matrices, all dxd
+mat_m lin_comb_mats(const vec_m& co, const vector<mat_m>& mats)
+{
+  int n = mats.size(), d = mats[0].nrows();
+  mat_m a(d,d);
+  for (int i=0; i<n; i++)
+    {
+      ZZ c = co[i+1];
+      if (c!=0)
+        a += c*mats[i];
+    }
+  return a;
+}
+
+// Linear combinarion of n>0 matrices, all dxd
+mat_m lin_comb_mats(const vector<ZZ>& co, const vector<mat_m>& mats)
+{
+  int n = mats.size(), d = mats[0].nrows();
+  mat_m a(d,d);
+  for (int i=0; i<n; i++)
+    {
+      ZZ c = co[i];
+      if (c!=0)
+        a += c*mats[i];
+    }
+  return a;
+}
+
+// same as m.output(cout) except no newlines between rows
+template<class T>
+void output_flat_matrix(const Zmat<T>& m, ostream&s)
+{
+  vector<T> entries = m.get_entries();
+  auto mij = entries.begin();
+  s << "[";
+  long nr = m.nrows();
+  while(nr--)
+    {
+      long nc = m.ncols();
+      s<<"[";
+      while(nc--)
+        {
+          s<<(*mij++);
+          if(nc)
+            s<<",";
+        }
+      s<<"]";
+      if(nr)
+        s<<",";
+    }
+  s << "]";
+}
+
+// rank of an NTL matrix:
+long rank(mat_ZZ A)
+{
+  ZZ d2;
+  return NTL::image(d2, A);
+}
+
+// nullity of an NTL matrix:
+long nullity(mat_ZZ A)
+{
+  return A.NumRows()-rank(A);
+}
+
 ///////////////////////////////////////////////////////////////////////////
 
 
@@ -1878,9 +2089,9 @@ template Zmat<int> addscalar<int>(const Zmat<int>&, const int&);
 template Zvec<int> apply<int>(const Zmat<int>&, const Zvec<int>&);
 template int inverse<int>(const Zmat<int>&, Zmat<int>&);
 template int liftmat<int>(const Zmat<int>& mm, const int& pr, Zmat<int>& m, int& dd);
-
 template void Zvec<int>::sub_row(const Zmat<int>& m, int i);
 template void Zvec<int>::add_row(const Zmat<int>& m, int i);
+template void output_flat_matrix<int>(const Zmat<int>& m, ostream&s);
 
 // Instantiate Zmat template functions for T=long
 template void add_row_to_vec<long>(Zvec<long>& v, const Zmat<long>& m, long i);
@@ -1937,9 +2148,9 @@ template Zmat<long> addscalar<long>(const Zmat<long>&, const long&);
 template Zvec<long> apply<long>(const Zmat<long>&, const Zvec<long>&);
 template long inverse<long>(const Zmat<long>&, Zmat<long>&);
 template int liftmat<long>(const Zmat<long>& mm, const long& pr, Zmat<long>& m, long& dd);
-
 template void Zvec<long>::sub_row(const Zmat<long>& m, int i);
 template void Zvec<long>::add_row(const Zmat<long>& m, int i);
+template void output_flat_matrix<long>(const Zmat<long>& m, ostream&s);
 
 // Instantiate Zmat template functions for T=ZZ
 template void add_row_to_vec<ZZ>(Zvec<ZZ>& v, const Zmat<ZZ>& m, long i);
@@ -1996,6 +2207,7 @@ template Zmat<ZZ> addscalar<ZZ>(const Zmat<ZZ>&, const ZZ&);
 template Zvec<ZZ> apply<ZZ>(const Zmat<ZZ>&, const Zvec<ZZ>&);
 template ZZ inverse<ZZ>(const Zmat<ZZ>&, Zmat<ZZ>&);
 template int liftmat<ZZ>(const Zmat<ZZ>& mm, const ZZ& pr, Zmat<ZZ>& m, ZZ& dd);
-
 template void Zvec<ZZ>::sub_row(const Zmat<ZZ>& m, int i);
 template void Zvec<ZZ>::add_row(const Zmat<ZZ>& m, int i);
+template void output_flat_matrix<ZZ>(const Zmat<ZZ>& m, ostream&s);
+
