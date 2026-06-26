d539078e3eb90aae91913de60eccf169067d676a
John Cremona <john.cremona@gmail.com>
2026-06-26 08:43:03 +0100

    increase default maxnp in makensp

--- progs/makensp.cc
+++ progs/makensp.cc
@@ -71,7 +71,8 @@ int main()
     // The Newspace constructor looks for a suitable splitting
     // operator which is a linear combination of up to maxnp prime
     // Hecke operators with coefficients up to maxc:
-    int maxnp = 15, maxc = 100, minp=2;
+    int maxnp = 20, maxc = 100, minp=2;
+    maxnp = min (maxnp, nap + 5); //H1->npdivs);
     Newspace NS(H1, maxnp, maxc, minp, verbose);
     if (cdim>0 && !NS.ok())
       {
